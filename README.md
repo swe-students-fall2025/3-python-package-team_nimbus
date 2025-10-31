6c8510311a40589e5b8f3458be3a5c9c974f3868
Mojin-Yuan <my2384@nyu.edu>
2025-10-31 17:25:56 -0400

    Add test program and readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Python Package Exercise
+# 🎰 nimbuscasino
+[![Build & Test](https://github.com/swe-students-fall2025/3-python-package-team_nimbus/actions/workflows/python-package.yml/badge.svg)](https://github.com/swe-students-fall2025/3-python-package-team_nimbus/actions/workflows/python-package.yml)
 
-An exercise to create a Python package, build it, test it, distribute it, and use it. See [instructions](./instructions.md) for details.
+**nimbuscasino** is a lightweight Python package offering a collection of fun, chance-based mini-games such as Rock-Paper-Scissors, Coin Flip, Roulette, and Slots. It’s designed for developers who want to add playful interactions or short mental breaks into their applications, while keeping the logic deterministic, testable, and credit-agnostic. Each function is pure (no I/O or global state), returns structured outcomes and payouts, and can be easily integrated into simulations, chatbots, or playful apps.
+
+⚠️ *This package is for entertainment and educational use only — no real gambling or monetary transactions are involved.*
+
+---
+
+## 📦 Installation
+```bash
+pip install nimbuscasino
+
---
+++ tests/rpstest.py
@@ -0,0 +1,44 @@
+import pytest
+from nimbuscasino import rps 
+
+
+class Tests:
+    #
+    # Fixtures - these are functions that can do any optional setup or teardown before or after a test function is run.
+    #
+
+    @pytest.fixture
+    def example_fixture(self):
+        """
+        An example of a pytest fixture - a function that can be used for setup and teardown before and after test functions are run.
+        """
+
+
+    
+        # place any setup you want to do before any test function that uses this fixture is run
+
+        yield  # at th=e yield point, the test function will run and do its business
+
+        # place with any teardown you want to do after any test function that uses this fixture has completed
+
+    #
+    # Test functions
+    #
+
+
+def test_sanity_check(self, example_fixture):
+        """
+        Test debugging... making sure that we can run a simple test that always passes.
+        Note the use of the example_fixture in the parameter list - any setup and teardown in that fixture will be run before and after this test function executes
+        From the main project directory, run the `python3 -m pytest` command to run all tests.
+        """
+        expected = True  # the value we expect to be present
+        actual = True  # the value we see in reality
+        winorlose, bet = rps.rps('rock')
+        if winorlose == 'win':
+            assert bet == 1
+        elif winorlose == 'lose':
+            assert bet == -1
+        elif winorlose == 'tie':
+            assert bet == 0
+            
